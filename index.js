9a21ebfc891b3cacbe103b2ab2fcffa8de7e571d
Shivani Bhakta <sdpatel0612@gmail.com>
2023-12-19 09:51:20 -0600

    updated index.js

--- index.js
+++ index.js
@@ -27,32 +27,44 @@ async function checkWeather(city) {
       "Also, it's cloudy. Some type of coat is recommended for rain.";
   } else if (data.weather[0].main == "Clear") {
     weatherIcon.src = "weatherImages/clear.png";
+    document.body.style.backgroundImage =
+      "url('https://images.freeimages.com/images/large-previews/e62/clear-sky-1363026.jpg?fmt=webp&w=500')";
     document.querySelector(".icon-details").innerHTML =
       "Also, it's a sunny day for shades!";
   } else if (data.weather[0].main == "Rain") {
     weatherIcon.src = "weatherImages/rain.png";
+    document.body.style.backgroundImage =
+      "url('https://www.pewtrusts.org/-/media/post-launch-images/2020/03/gettyimages838815210jpgmaster/16x9_m.jpg')";
     document.querySelector(".icon-details").innerHTML =
       "Also, it looks like you'll need an umbrella.";
   } else if (data.weather[0].main == "Drizzle") {
     weatherIcon.src = "weatherImages/drizzle.png";
+    document.body.style.backgroundImage =
+      "url('https://cdn.windy.app/site-storage/posts/October2023/nature-grass-snow-fog-mist-morning-41797-pxhere.com.jpg')";
     document.querySelector(".icon-details").innerHTML =
       "Also, you might want to bring an umbrella or raincoat.";
   } else if (data.weather[0].main == "Mist") {
     weatherIcon.src = "weatherImages/mist.png";
+    document.body.style.backgroundImage =
+      "url('https://images.unsplash.com/photo-1528015195429-9ef1e5111b38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE3fHx8ZW58MHx8fHx8&w=1000&q=80')";
   } else if (data.weather[0].main == "Snow") {
     weatherIcon.src = "weatherImages/snow.png";
+    document.body.style.backgroundImage =
+      "url('https://www.google.com/url?sa=i&url=http%3A%2F%2Fffden-2.phys.uaf.edu%2F211_fall2013.web.dir%2FSean_Counihan%2FSnowfall.html&psig=AOvVaw2NG5VOd7kDL5EglkWYdVa3&ust=1703085630073000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOjK7sXmm4MDFQAAAAAdAAAAABAD')";
     document.querySelector(".icon-details").innerHTML =
       "Also, make sure you're wearing gloves so you can enjoy a good snowball fight!";
-  } else if (data.weather[0].main == "Wind") {
-    weatherIcon.src = "weatherImages/wind.png";
-    if (data.wind.speed > 5) {
-      document.querySelector(".wind-details").innerHTML =
-        "Also, it's a bit windy, so dress in layers would be beneficial.";
-    }
   } else if (data.weather[0].main == "Haze") {
     weatherIcon.src = "weatherImages/wind.png";
+    document.body.style.backgroundImage =
+      "url('https://uknow.uky.edu/sites/default/files/styles/uknow_story_image/public/GettyImages-1272193101.jpg')";
+    document.querySelector(".wind-details").innerHTML =
+      "And it's a bit hazy. Wearing a mask is advised.";
   } else if (data.weather[0].main == "Smoke") {
     weatherIcon.src = "weatherImages/clouds.png";
+    document.body.style.backgroundImage =
+      "url('https://uknow.uky.edu/sites/default/files/styles/uknow_story_image/public/GettyImages-1272193101.jpg')";
+    document.querySelector(".wind-details").innerHTML =
+      "And wearing a mask would be a good idea.";
   }
   if (data.main.temp >= 0 && data.main.temp <= 15) {
     document.querySelector(".temp-details").innerHTML =
@@ -66,6 +78,14 @@ async function checkWeather(city) {
   } else if (data.main.temp > 30) {
     document.querySelector(".temp-details").innerHTML =
       "Wear light fabrics. A skirt or maxi dress would be perfect. Alternatively, a short sleeve t shirt and jeans or shorts. Sneakers, sandals, open toed shoes are a much better option than heavy boots. Think of indoor air conditioning and take with you a cardigan! ";
+  } else if (data.weather[0].main == "Wind") {
+    weatherIcon.src = "weatherImages/wind.png";
+    document.body.style.backgroundImage =
+      "url('https://i0.wp.com/qcostarica.com/wp-content/uploads/2022/02/viento-winds.jpg?resize=696%2C450&ssl=1')";
+    if (data.wind.speed > 5) {
+      document.querySelector(".wind-details").innerHTML =
+        "Also, it's a bit windy, so dressing in layers is advised.";
+    }
   }
 
   document.querySelector(".weather").style.display = "block";
